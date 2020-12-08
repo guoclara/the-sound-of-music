124d08bf77a65805fe4fba59cdd2568c8c57c44d
David Moon <dmoon@Davids-MBP.home>
2020-12-08 00:30:17 -0600

    add requirements and finished UI

--- scripts/app.py
+++ scripts/app.py
@@ -1,22 +1,18 @@
 # This script is the UI for the project (supports Python 3.8+)
-# To run this file: streamlit run app.py
-# Type ctrl+c in the terminal to close the web browser
 # NOTE: you must clone the Blink-1470 repo (https://github.com/damoon843/Blink-1470) to have display images/audio files
 import numpy as np
 import pandas as pd
 import streamlit as st
 import os
-from PIL import Image
 
 st.title("Blink-1470: Sound of Music 🎵🎧")
 st.header("*An Interpretable CNN Model for Genre Classification*")
 
 "---"
 
-st.subheader("The Basics...")
 st.markdown(
     """
-    There are **10 genres** that our model seeks to classify:  
+    There are **10 genres** that this model seeks to classify:  
 	1. Blues  
 	2. Jazz  
 	3. Classical  
@@ -32,33 +28,20 @@ st.markdown(
 
 st.markdown(
     """
-    The general architecture of our model is as follows:  
-    *insert model description here*  
-    *use keras plot_model to visualize*
+    **NOTE:** please refer to the poster for more specifics on model architecture, implementation details, etc (found in the repo).
     """
     )
 
-st.markdown(
-    """
-    This model implements a masking layer to filter out noisy activations.
-    Here's an example of a single mask, as well as an 
-    input feature map's activations before & after its application:    
-    *use np colormaps to visualize here*
-    """
-    )
-
-st.subheader("Interpreting Our Model...")
+st.subheader("Explore our Results")
 
 # Setup for displaying media
 abspath = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/')
 
 st.markdown(
 	"""
-	Feel free to explore the differences in .wav files and activations between the 
+	Feel free to analyze the differences in .wav files and deconvolved activations between the 
 	masked and unmasked models. You can also compare the differences in sound and 
-	activations between a single filter of a model's first convolution layer.    
-	*do we want filters after second conv layer?*  
-    *do we want to add original audio here?*
+	activations between a single filter (out of 32) of a model's first convolution layer.    
 	"""
 )
 
@@ -66,83 +49,39 @@ genre_type = st.selectbox("Which genre would you like to explore?",
                           ("Blues", "Jazz", "Classical", "Metal", "Country",
                            "Pop", "Disco", "Reggae", "Hiphop", "Rock"))
 
-if genre_type == "Blues":
-    masked, unmasked = st.beta_columns(2)
-    with masked:
-        st.write("**Masked model**")
-        masked_deconv_blues_image = os.path.join(abspath, 'masked/masked_deconv_blues.png')
-        try:
-            st.image(masked_deconv_blues_image)
-        except FileNotFoundError:
-            st.write("Error: no image at this path")
-        
-        masked_deconv_blues_audio = os.path.join(abspath, 'masked/masked_deconv_blues.wav')
-        try:
-            st.audio(masked_deconv_blues_audio)
-        except FileNotFoundError:
-            st.write("Error: no audio at this path")
-            
-        st.write("Filter 16 (out of 32) after 1st conv layer:")
-        masked_deconv_blues_filter_image = os.path.join(abspath, 'masked/masked_conv1_filter16_blues.png')
-        try:
-            st.image(masked_deconv_blues_filter_image)
-        except FileNotFoundError:
-            st.write("Error: no image at this path")
-        
-        masked_deconv_blues_filter_audio = os.path.join(abspath, 'masked/masked_conv1_filter16_blues.wav')
-        try:
-            st.audio(masked_deconv_blues_filter_audio)
-        except FileNotFoundError:
-            st.write("Error: no audio at this path")
-            
-    with unmasked:
-        st.write("**Unmasked model**")
-        unmasked_deconv_blues_image = os.path.join(abspath, 'unmasked/deconv_blues.png')
-        try:
-            st.image(unmasked_deconv_blues_image)
-        except FileNotFoundError:
-            st.write("Error: no image at this path")
-        
-        unmasked_deconv_blues_audio = os.path.join(abspath, 'unmasked/deconv_blues.wav')
-        try:
-            st.audio(unmasked_deconv_blues_audio)
-        except FileNotFoundError:
-            st.write("Error: no audio at this path")
-        
-        st.write("Filter 16 (out of 32) after 1st conv layer:")
-        masked_deconv_blues_filter = os.path.join(abspath, 'unmasked/conv1_filter16_blues.png')
-        try:
-            st.image(masked_deconv_blues_filter)
-        except FileNotFoundError:
-            st.write("Error: no image at this path")
-        
-        unmasked_deconv_blues_filter_audio = os.path.join(abspath, 'unmasked/conv1_filter16_blues.wav')
-        try:
-            st.audio(unmasked_deconv_blues_filter_audio)
-        except FileNotFoundError:
-            st.write("Error: no audio at this path")
+masked, unmasked = st.beta_columns(2)
+genre_type = genre_type.lower() # Lowercase for templating
 
-# TODO: insert media for all other genres
-elif genre_type == "Jazz":
-    pass
-elif genre_type == "Classical":
-    pass
-elif genre_type == "Metal":
-    pass
-elif genre_type == "Country":
-    pass
-elif genre_type == "Pop":
-    pass
-elif genre_type == "Disco":
-    pass
-elif genre_type == "Reggae":
-    pass
-elif genre_type == "Hiphop":
-    pass
-elif genre_type == "Rock":
-    pass
+with masked:
+    st.write("**Masked model**")
+    masked_deconv_image = os.path.join(abspath, f'masked/masked_deconv_{genre_type}.png')
+    st.image(masked_deconv_image)
+    
+    masked_deconv_audio = os.path.join(abspath, f'masked/masked_deconv_{genre_type}.wav')
+    st.audio(masked_deconv_audio)
+        
+    st.write("Filter 16 after 1st conv layer:")
+    masked_deconv_filter_image = os.path.join(abspath, f'masked/masked_conv1_filter16_{genre_type}.png')
+    st.image(masked_deconv_filter_image)
+    
+    masked_deconv_filter_audio = os.path.join(abspath, f'masked/masked_conv1_filter16_{genre_type}.wav')
+    st.audio(masked_deconv_filter_audio)
+with unmasked:
+    st.write("**Unmasked model**")
+    unmasked_deconv_image = os.path.join(abspath, f'unmasked/deconv_{genre_type}.png')
+    st.image(masked_deconv_image)
+    
+    unmasked_deconv_audio = os.path.join(abspath, f'unmasked/deconv_{genre_type}.wav')
+    st.audio(unmasked_deconv_audio)
+        
+    st.write("Filter 16 after 1st conv layer:")
+    unmasked_deconv_filter_image = os.path.join(abspath, f'unmasked/conv1_filter16_{genre_type}.png')
+    st.image(unmasked_deconv_filter_image)
+    
+    unmasked_deconv_filter_audio = os.path.join(abspath, f'unmasked/conv1_filter16_{genre_type}.wav')
+    st.audio(unmasked_deconv_filter_audio)
 
-st.subheader("References...")
+st.subheader("References")
 st.write(
     """
     The repo for this project can be found [here](https://github.com/damoon843/Blink-1470)  
